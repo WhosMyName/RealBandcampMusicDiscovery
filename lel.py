fc95267085faff45f02f02e716ecc81fd4f60c33
WhosMyName <WhosMyName@users.noreply.github.com>
2018-09-19 02:42:47 +0200

    Create lel.py

---
+++ lel.py
@@ -0,0 +1,49 @@
+from brain import data
+import srsly
+from stop import this_sit
+
+#Le big gud url to get tags
+#https://bandcamp.com/tags
+
+generic_data = """
+git this:
+
+
+    <h1>tags</h1>
+    <div class="tagcloud" id="tags_cloud">
+        <a href="/tag/electronic" class="tag size1">electronic</a>
+        <a href="/tag/experimental" class="tag size1">experimental</a>
+"""
+# They have way to many whitespaces
+
+def use_tags_to("discover"):
+    pass
+
+#those are *all* tags so filter them
+
+#WIP
+
+#query the data in this format
+
+
+#https://bandcamp.com/api/discover/3/get_web?g=jazz&g=ambient&s=top&p=0&gn=0&f=all&w=0
+#                                            ^
+#--------------------------------------------| dis only works for subgenre
+
+
+#not sure for what those are used
+#&lo=true
+#&lo_action_url=https://bandcamp.com
+
+#load to *???* and then compare
+
+#quengel api to got em
+
+some_moar = """
+url_hints	
+slug	"death-spells"
+item_type	"a"
+subdomain	"holyfawn"
+custom_domain_verified	null
+"""
+#is a dict btw
