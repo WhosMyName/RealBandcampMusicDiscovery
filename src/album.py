af92f3d48c393aaa4514ac2e95fa378d45b27977
Who is my Name! <chikangwu95@gmail.com>
2022-12-06 14:16:50 +0100

    removed main()

--- src/album.py
+++ src/album.py
@@ -13,31 +13,13 @@ class Album():
         self.cover_url: str = cover_url
 
     def __str__(self):
-        return "%s - %s" % (self.band, self.name)
+        return f"{self.band} - {self.name}"
 
     def __repr__(self):
-        return "%s - %s" % (self.__class__, str(self.__dict__))
+        return f"{self.__class__} - {str(self.__dict__)}"
 
     def __hash__(self):
         return hash((self.band, self.name))
 
     def __eq__(self, other):
         return self.__hash__() == other.__hash__()
-
-
-def __main__():
-    alb = Album("GenericALbum.exe", "https://github.com/whosmyname", "datboigud",
-                "https://i.pinimg.com/originals/5e/40/5b/5e405be0320863d04c84b399dc2969ca.jpg")
-    alb2 = Album("GenericALbum.exe", "https://github.com/whosmyname", "datboigud",
-                 "https://i.pindfzguhiojkplöimg.com/originals/5e/40/5b/5e405be0320863d04c84b399dc2969ca.jpg")
-    print(alb.__hash__())
-    print(alb2.__hash__())
-    print(alb.__hash__() == alb2.__hash__())
-    sed = set()
-    sed.add(alb)
-    sed.add(alb2)
-    print(sed)
-
-
-if __name__ == "__main__":
-    __main__()
