4f3f322912760a35e2d4ae562abfc29cd2a72fdd
Who is my Name! <chikangwu95@gmail.com>
2022-12-09 18:11:44 +0100

    hopefully handled the consecutive dot issue

--- src/webconnector.py
+++ src/webconnector.py
@@ -131,9 +131,9 @@ class Connector(multiprocessing.Process):
     def download_albums(self, msg):
         """ downloads all albums from message """
         albumlist = msg.get_data()
-        danger_chars = "[\/*?:\"<>|~°^.]"
+        danger_chars = "[\/*?:\"<>|~°^]"
         for album in albumlist:
-            location = f"Albums{PATHSEP}{re_sub(danger_chars, '_', album.__str__())}{PATHSEP}"
+            location = f"Albums{PATHSEP}{re_sub(danger_chars, '_', album.__str__()).strip('.')}{PATHSEP}"
             if not os.path.exists(location):
                 os.makedirs(location)
             if album.cover:
