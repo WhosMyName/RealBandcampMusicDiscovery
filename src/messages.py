f08e055d2394ce92fd6339b40b499dbc5fadffba
Who is my Name! <chikangwu95@gmail.com>
2022-12-25 16:56:16 +0100

    added new Progress Message + default args + __str__

--- src/messages.py
+++ src/messages.py
@@ -18,58 +18,70 @@ class Msg():
         """ returns data """
         return self.data
 
+    def __str__(self) -> str:
+        "returns string representation of message object"
+        return f"{self.sender} sent {self.__class__.__name__} with: {self.data}"
+
 
 class MsgGetTags(Msg):
     """ class to set signal for getting all tags """
 
-    def __init__(self, data):
+    def __init__(self, data=None):
         super().__init__(data)
 
 
 class MsgPutTags(Msg):
     """ class to deliver tags """
 
-    def __init__(self, data):
+    def __init__(self, data=None):
         super().__init__(data)
 
 
 class MsgPutFetchTags(Msg):
     """ class to deliver fetch tags """
 
-    def __init__(self, data):
+    def __init__(self, data=None):
         super().__init__(data)
 
 
 class MsgPutAlbums(Msg):
     """ class to deliver albums """
 
-    def __init__(self, data):
+    def __init__(self, data=None):
         super().__init__(data)
 
 
 class MsgDownloadAlbums(Msg):
     """ class to download albums """
 
-    def __init__(self, data):
+    def __init__(self, data=None):
         super().__init__(data)
 
 
 class MsgFinishedDownloads(Msg):
     """ class to download albums """
 
-    def __init__(self, data):
+    def __init__(self, data=None):
+        super().__init__(data)
+
+
+class MsgSetProgress(Msg):
+    """indicator message to (re-)set progress of operations"""
+
+    def __init__(self, curr: int = 0, max: int = 0, mes: str = ""):
+        data: dict = {"curr": curr, "max": max, "message": mes}
         super().__init__(data)
 
 
 class MsgPause(Msg):
     """ class to signal pause of current fetch cycle for albums """
 
-    def __init__(self, data):
+    def __init__(self, data=None):
         super().__init__(data)
 
 
 class MsgQuit(Msg):
     """ class to signal the stop of program execution """
 
-    def __init__(self, data):
+    def __init__(self, data=None):
         super().__init__(data)
