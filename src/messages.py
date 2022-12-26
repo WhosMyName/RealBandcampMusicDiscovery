d021aec061444f0bc9d58892a0a2461976b88276
Who is my Name! <chikangwu95@gmail.com>
2022-12-26 16:28:25 +0100

    type hints (although i don't see them working qwq)

--- src/messages.py
+++ src/messages.py
@@ -1,11 +1,12 @@
 """ Lé big [x]mq rip off aka classes to replace old event based IPC system that didn't worked (redit: as expected :rollseyes:) """
 
+from album import Album
 # give this bic boii a gud think
 
 class Msg():
     """ Base Class for all Msg-Types """
 
-    def __init__(self, data=None):
+    def __init__(self, data = None):
         """ simple init """
         self.sender: str = "."
         self.data = data
@@ -30,42 +31,42 @@ class Msg():
 class MsgGetTags(Msg):
     """ class to set signal for getting all tags """
 
-    def __init__(self, data=None):
+    def __init__(self, data : None = None):
         super().__init__(data)
 
 
 class MsgPutTags(Msg):
     """ class to deliver tags """
 
-    def __init__(self, data=None):
+    def __init__(self, data : list[str] = None):
         super().__init__(data)
 
 
 class MsgPutFetchTags(Msg):
     """ class to deliver fetch tags """
 
-    def __init__(self, data=None):
+    def __init__(self, data : list[str] = None):
         super().__init__(data)
 
 
 class MsgPutAlbums(Msg):
     """ class to deliver albums """
 
-    def __init__(self, data=None):
+    def __init__(self, data: list[Album] = None):
         super().__init__(data)
 
 
 class MsgDownloadAlbums(Msg):
     """ class to download albums """
 
-    def __init__(self, data=None):
+    def __init__(self, data: list[Album] = None):
         super().__init__(data)
 
 
 class MsgFinishedDownloads(Msg):
     """ class to download albums """
 
-    def __init__(self, data=None):
+    def __init__(self, data : None = None):
         super().__init__(data)
 
 
@@ -80,12 +81,12 @@ class MsgSetProgress(Msg):
 class MsgPause(Msg):
     """ class to signal pause of current fetch cycle for albums """
 
-    def __init__(self, data=None):
+    def __init__(self, data: None = None):
         super().__init__(data)
 
 
 class MsgQuit(Msg):
     """ class to signal the stop of program execution """
 
-    def __init__(self, data=None):
+    def __init__(self, data: None = None):
         super().__init__(data)
